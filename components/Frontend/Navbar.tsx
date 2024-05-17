9b9f87f288b56427b13b1a30cd86546bb6e06ca8
Fuma1322 <leonardfuma26@gmail.com>
2024-05-17 16:30:22 +0200

    dashboard

--- components/Frontend/Navbar.tsx
+++ components/Frontend/Navbar.tsx
@@ -5,7 +5,7 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'About', href: '#' },
+  { name: 'About', href: 'dashboard' },
   { name: 'Services', href: '#' },
   { name: 'Socials', href: '#' },
   { name: 'Contact Us', href: '#' },
@@ -19,14 +19,7 @@ export default function Navbar() {
       <header className="absolute inset-x-0 top-2 z-50 h-auto">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-               <span className="sr-only">Your Company</span>
-              <img
-                className="h-10 w-auto"
-                src="/logo.png"
-                alt=""
-              />
-            </a>
+          <h2 className='font-bold sm:text-6xl text-indigo-600'>ClinicEase</h2>
           </div>
           <div className="flex lg:hidden">
             <button
@@ -55,14 +48,7 @@ export default function Navbar() {
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="/logo.png"
-                  alt=""
-                />
-              </a>
+            <h2 className='font-bold sm:text-6xl text-3xl text-indigo-600'>ClinicEase</h2>
               <button
                 type="button"
                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -149,3 +135,4 @@ export default function Navbar() {
     </div>
   )
 }
+
