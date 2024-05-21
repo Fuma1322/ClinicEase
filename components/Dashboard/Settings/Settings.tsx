d085314f9a47a3467f828db77897ad266687e7b1
Fuma1322 <leonardfuma26@gmail.com>
2024-05-21 14:29:01 +0200

    updates on Dashboard

--- components/Dashboard/Settings/Settings.tsx
+++ components/Dashboard/Settings/Settings.tsx
@@ -1,5 +1,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import GeneralSettings from "./GeneralSettings";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 
 export default function Settings() {
     const tabs = [
@@ -35,34 +37,35 @@ export default function Settings() {
         },
     ];
   return (
-      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
-        <div className="mx-auto grid w-full max-w-6xl gap-2">
-          <h1 className="text-3xl font-semibold">Settings</h1>
-        </div>
-        <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-          <div className="">
-          <Tabs defaultValue="general" className="w-[400px]">
-            <TabsList>
-            {tabs.map((tab)=>{
+    <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+    <Tabs defaultValue="general">
+      <div className="flex items-center">
+        <TabsList>
+        {tabs.map((tab)=>{
             return(
                 <TabsTrigger key={tab.value} value={tab.value}>
                   {tab.label}
                 </TabsTrigger>
               )
              })}
-            </TabsList>
-            {
-              tabs.map((tab) =>{
-                return(
-                  <TabsContent className="w-full" value={tab.value} key={tab.value}>
-                    {tab.component}
-                  </TabsContent>
-                );
-              })
-            } 
-          </Tabs>
-          </div>
+        </TabsList>
+        <div className="ml-auto flex items-center gap-2">
+          <Button size="sm" className="h-8 gap-1">
+            <PlusCircle className="h-3.5 w-3.5" />
+            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+              Add Product
+            </span>
+          </Button>
         </div>
-      </main>
+      </div>
+      {tabs.map((tab) =>{
+        return(
+          <TabsContent className="w-full" value={tab.value} key={tab.value}>
+            {tab.component}
+          </TabsContent>
+        );
+              })} 
+    </Tabs>
+  </main>
   )
 }
