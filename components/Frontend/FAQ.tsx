3dcba5095cf344fb18a89cd1cf5193aec713c920
Fuma1322 <leonardfuma26@gmail.com>
2024-05-28 13:25:47 +0200

    debug

--- components/Frontend/FAQ.tsx
+++ components/Frontend/FAQ.tsx
@@ -4,15 +4,15 @@ import React, { useState } from "react";
 
 const Accordion = () => {
   return (
-    <section className="relative z-20 overflow-hidden bg-black pb-12 pt-20 dark:bg-dark lg:pb-[90px] lg:pt-[120px]">
+    <section className="relative z-20 overflow-hidden bg-black pb-12 pt-20 dark:bg-dark lg:pb-[90px] lg:pt-[120px] mb-6">
       <div className="container mx-auto">
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
             <div className="mx-auto mb-[60px] max-w-[520px] text-center lg:mb-20">
-              <span className="mb-2 block text-lg font-semibold text-primary">
+              <span className="mb-2 block text-lg font-semibold text-gray-400">
                 FAQ
               </span>
-              <h2 className="mb-4 text-3xl font-bold text-dark dark:text-white sm:text-[40px]/[48px]">
+              <h2 className="mb-4 text-3xl font-bold text-dark dark:text-gray-400 sm:text-[40px]/[48px]">
                 Any Questions? Look Here
               </h2>
               <p className="text-base text-body-color dark:text-dark-6">
@@ -126,7 +126,7 @@ type AccordionItemProps = {
         </div>
 
         <div className="w-full">
-          <h4 className="mt-1 text-lg font-semibold text-dark dark:text-white">
+          <h4 className="mt-1 text-lg font-semibold text-dark dark:text-gray-400">
             {header}
           </h4>
         </div>
