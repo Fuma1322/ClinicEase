59e9a3944fe0f651ac2fb0102b2b206dd6a98aa9
Fuma1322 <leonardfuma26@gmail.com>
2024-05-29 16:32:42 +0200

    wenesday

---
+++ components/Dashboard/ClinicDashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ClinicDashboard() {
+  return (
+    <div>
+        <div className="h2">Hello</div>
+    </div>
+  )
+}
---
+++ components/Dashboard/UserDashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function UserDashboard() {
+  return (
+    <div>
+        <div className="h2">HelloUser</div>
+    </div>
+  )
+}
