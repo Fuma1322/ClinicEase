fda900934f436440807a6473b3036bb479531a42
Fuma1322 <leonardfuma26@gmail.com>
2024-05-31 16:27:12 +0200

    Registration Form

--- app/(front)/register/page.tsx
+++ app/(front)/register/page.tsx
@@ -1,9 +1,18 @@
+import RegisterForm from '@/components/Auth/RegisterForm'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>
-        <h2>Register Page</h2>
+    <div className="min-h-screen py-8 mt-24">
+        <div className="grid md:grid-cols-2 grid-cols-1 w-full overflow-hidden max-w-5xl mx-auto bg-white border border-gray-200 rounded-lg shadow  dark:bg-black dark:border-gray-700">
+            <div className="hidden md:flex linear-bh">
+                {/*IMAGE*/}
+            </div>
+            <div className="">
+                {/*FORM*/}
+                <RegisterForm />
+            </div>
+        </div>
     </div>
   )
 }
---
+++ components/Auth/RegisterForm.tsx
@@ -0,0 +1,124 @@
+import Link from "next/link";
+
+export default function RegisterForm() {
+    return (
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              className="mx-auto h-16 w-auto"
+              src="/logo.png"
+              alt="Your Company"
+            />
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-400">
+              Create New Account
+            </h2>
+          </div>
+  
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label htmlFor="fname" className="block text-sm font-medium leading-6 text-gray-400">
+                  First Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="fname"
+                    name="fname"
+                    type="text"
+                    autoComplete="fname"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="lname" className="block text-sm font-medium leading-6 text-gray-400">
+                  Last Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="lname"
+                    name="lname"
+                    type="text"
+                    autoComplete="lname"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-400">
+                  Email Address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="pnum" className="block text-sm font-medium leading-6 text-gray-400">
+                  Phone Number
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="pnum"
+                    name="pnum"
+                    type="text"
+                    autoComplete="pnum"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <div className="flex items-center justify-between">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-400">
+                    Password
+                  </label>
+                  <div className="text-sm">
+                    <a href="#" className="font-semibold text-sky-400 hover:text-indigo-500">
+                      Forgot password?
+                    </a>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+  
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-sky-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+  
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Already have Account?{' '}
+              <Link
+               href="/login" 
+               className="font-semibold leading-6 text-sky-400 hover:text-indigo-500">
+                Log In
+                </Link>
+            </p>
+          </div>
+        </div>
+    )
+  }
