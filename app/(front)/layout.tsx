f2f8d5eadbbe250987df5b994bf20905fa45acf9
entsoereng <entsoereng@aol.com>
2024-06-17 16:10:50 +0200

    practise

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -1,12 +1,16 @@
 import Footer from '@/components/Frontend/Footer'
 import Navbar from '@/components/Frontend/Navbar'
 import { SiteHeader } from '@/components/site-header'
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 import React, { ReactNode } from 'react'
 
-export default function Layout({children}:{children:ReactNode}) {
+export default async function Layout({children}:{children:ReactNode}) {
+  const session = await getServerSession(authOptions);
   return (
     <div className='bg-black'>
-     <SiteHeader />
+     <SiteHeader session={session} />
     {children}
     <Footer/>
     </div>
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,5 +1,5 @@
+"use client"
 import Link from "next/link"
-
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { CommandMenu } from "@/components/command-menu"
@@ -9,8 +9,27 @@ import { MobileNav } from "@/components/mobile-nav"
 import ModeToggle  from "@/components/ModeToggle"
 import { Button, buttonVariants } from "@/components/ui/button"
 import { LogInIcon, Mail } from "lucide-react"
+import { Session } from "next-auth"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { signOut } from "next-auth/react"
+import { useRouter } from "next/navigation"
+import { PersonIcon } from "@radix-ui/react-icons"
 
-export function SiteHeader() {
+export function SiteHeader({session}:{session:Session|null}) {
+  const user = session?.user;
+  const router = useRouter();
+  async function handleLogout() {
+    await signOut()
+    router.push("/login");
+  }
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
@@ -21,12 +40,35 @@ export function SiteHeader() {
             <CommandMenu />
           </div>
           <nav className="flex items-center gap-4">
-          <Button asChild>
-            <Link href={"/login"}>
-            <LogInIcon className="mr-2 h-4 w-4" /> Login
-            </Link>
-          </Button>
-            <ModeToggle />
+            {session && session.user && user?.email?(
+                   <DropdownMenu>
+                   <DropdownMenuTrigger asChild>
+                       <Avatar className="cursor-pointer">
+                         {user.image ? (<AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" 
+                         />
+                        ) : (
+                           <AvatarFallback>CN</AvatarFallback>
+                        )}
+                       </Avatar>
+                   </DropdownMenuTrigger>
+                   <DropdownMenuContent align="end">
+                     <DropdownMenuLabel className="text-center">{user.name}</DropdownMenuLabel>
+                     <DropdownMenuLabel className="text-center font-light text-sm text-slate-500">{user.email}</DropdownMenuLabel>
+                     <DropdownMenuSeparator />
+                     <DropdownMenuItem>
+                      <Link href="/dashboard">Dashboard</Link>
+                      </DropdownMenuItem>
+                     <DropdownMenuItem onClick={()=> handleLogout()}>Logout</DropdownMenuItem>
+                   </DropdownMenuContent>
+                 </DropdownMenu>
+            ):(
+              <Button asChild>
+              <Link href={"/login"}>
+                <PersonIcon className="mr-2 h-4 w-4" /> Login
+              </Link>
+              </Button>
+            )}
+            <ModeToggle/>
           </nav>
         </div>
       </div>
